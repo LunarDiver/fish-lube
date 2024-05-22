a1f616a1ccba8546d27a670e5225bc39e3be054a
Luna <dev@lunardiver.com>
2024-05-22 04:24:56 +0200

    Fixed invalid settings

--- settings.lua
+++ settings.lua
@@ -1,8 +1,10 @@
 data:extend({
+    {
     type = "int-setting",
     name = "fl-setting",
     setting_type = "startup",
     default_value = 5,
     minimum_value = 1,
     maximum_value = 100
+    }
 })

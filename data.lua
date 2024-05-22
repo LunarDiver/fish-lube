410816193181218f78debc6f34bd4a57ee143ab0
Luna <dev@lunardiver.com>
2024-05-22 03:57:37 +0200

    Added tech

--- data.lua
+++ data.lua
@@ -15,5 +15,34 @@ local recipe = {
     result_count = 5,
     enabled = false
 }
-
-data:extend(recipe)
+local tech = {
+    type = "technology",
+    name = "fl-tech-lubricant",
+    icon = "__fish-lube__/graphics/tech-fish-lube.png",
+    icon_size = 256,
+    icon_mipmaps = 4,
+    unit = {
+        count = 25,
+        time = 15,
+        ingredients = {
+            {
+                name = "automation-science-pack",
+                amount = 1
+            },
+            {
+                name = "logistic-science-pack",
+                amount = 1
+            }
+        }
+    },
+    prerequisites = {
+        "automation-2"
+    },
+    effects = {
+        {
+            type = "unlock-recipe",
+            recipe = "fl-recipe-lubricant"
+        }
+    }
+}
+data:extend({ recipe, tech })
